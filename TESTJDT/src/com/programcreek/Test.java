d618d9e56a1ac877b8074a25ed3ab0bf87a2f3b9
KG00360770 <KG00360770@INNOSZPC01131.ATT.TechMahindra.com>
2017-01-17 22:09:41 +0530

    Some Message

---
+++ TESTJDT/src/com/programcreek/Test.java
@@ -0,0 +1,9 @@
+package com.programcreek;
+public class Test  {
+ private String name;
+private String age;
+private int value;
+public static void main(String[] args) {
+	System.out.println("Hello");
+}
+}
